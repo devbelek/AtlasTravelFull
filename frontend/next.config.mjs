b9a142996888f2a6fc111310052430fbe26279f5
devbelek <belekasrarov10@gmail.com>
2024-10-16 02:32:03 +0600

    df

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,21 @@
 import createNextIntlPlugin from 'next-intl/plugin';
- 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const withNextIntl = createNextIntlPlugin();
- 
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
- 
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
+  },
+};
+
 export default withNextIntl(nextConfig);
