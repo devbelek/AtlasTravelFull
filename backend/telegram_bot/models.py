938f30df63946c681e577e28acb76988c4eb1fb0
devbelek <belekasrarov10@gmail.com>
2024-10-18 07:39:28 +0600

    df

--- backend/telegram_bot/models.py
+++ backend/telegram_bot/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 
 class TelegramUser(models.Model):
-    chat_id = models.BigIntegerField(unique=True, verbose_name="Telegram Chat ID")
-    is_admin = models.BooleanField(default=False, verbose_name="Is Admin")
-    receive_inquiries = models.BooleanField(default=True, verbose_name="Receive Inquiries")
-    receive_reviews = models.BooleanField(default=True, verbose_name="Receive Reviews")
-    receive_about_us_inquiries = models.BooleanField(default=True, verbose_name="Receive 'About Us' Inquiries")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created At")
-    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated At")
+    chat_id = models.BigIntegerField(unique=True, verbose_name="Телеграм ID")
+    is_admin = models.BooleanField(default=False, verbose_name="Является администратором")
+    receive_inquiries = models.BooleanField(default=True, verbose_name="Получать запросы")
+    receive_reviews = models.BooleanField(default=True, verbose_name="Получать отзывы")
+    receive_about_us_inquiries = models.BooleanField(default=True, verbose_name="Получать запросы 'О нас'")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
 
     def __str__(self):
-        return f"Chat ID: {self.chat_id} (Admin: {self.is_admin})"
+        return f"Телеграм ID: {self.chat_id} (Администратор: {self.is_admin})"
 
     class Meta:
         verbose_name = 'Пользователь Телеграм'
