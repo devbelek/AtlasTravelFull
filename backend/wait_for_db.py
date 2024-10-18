9a28ddb163711574660ccb39021f3602f683295e
devbelek <belekasrarov10@gmail.com>
2024-10-18 07:27:53 +0600

    df

---
+++ backend/wait_for_db.py
@@ -0,0 +1,26 @@
+import sys
+import time
+import psycopg2
+from django.conf import settings
+
+
+def wait_for_db():
+    db_ready = False
+    while not db_ready:
+        try:
+            conn = psycopg2.connect(
+                dbname=settings.DATABASES['default']['NAME'],
+                user=settings.DATABASES['default']['USER'],
+                password=settings.DATABASES['default']['PASSWORD'],
+                host=settings.DATABASES['default']['HOST'],
+                port=settings.DATABASES['default']['PORT'],
+            )
+            conn.close()
+            db_ready = True
+        except psycopg2.OperationalError:
+            print("База данных недоступна, повторная попытка через 1 секунду...")
+            time.sleep(1)
+
+
+if __name__ == '__main__':
+    wait_for_db()
