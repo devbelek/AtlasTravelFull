3d599a2fbeb1913e7bb5f8d6168ad9d16bafd665
devbelek <belekasrarov10@gmail.com>
2024-10-19 18:24:16 +0600

    df

--- backend/atlas_travel/settings.py
+++ backend/atlas_travel/settings.py
@@ -149,9 +149,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/app/staticfiles'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
