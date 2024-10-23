9b7ddcea26b61af2c1f5084d4aef9fb1b68d94f0
devbelek <belekasrarov10@gmail.com>
2024-10-23 21:49:27 +0600

    df

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -2,9 +2,11 @@ import createMiddleware from "next-intl/middleware";
 
 export default createMiddleware({
     locales: ['ru', 'en', 'kg'],
-    defaultLocale: 'ru'
+    defaultLocale: 'ru',
+    localePrefix: 'always',
+    localeDetection: true
 });
 
 export const config = {
-  matcher: ["/((?!api|_next|.*\\..*).*)"],
+    matcher: ['/', '/(ru|en|kg)/:path*', '/((?!api|_next|_vercel|.*\\..*).*)']
 };
